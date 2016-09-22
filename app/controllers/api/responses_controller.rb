b8687c9d27ef5dcfeaae1505dd07f5b85ea61949
ashcza <ashcon.zand@gmail.com>
2016-09-22 09:38:52 -0700

    Added logic to responses controller

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -1,3 +1,15 @@
 class Api::ResponsesController < ApplicationController
 
+  def create
+    body = params['Body'].downcase
+    twiml = Twilio::TwiML::Response.new do |r|
+      if body == "hello"
+        r.Message "Hi!"
+      elsif body == "bye"
+        r.Message "Goodbye"
+      end
+    end
+    twiml.text
+  end
+
 end
