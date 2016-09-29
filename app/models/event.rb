0a02802185e57d9f6e62561a7fb7d226e5e5b9c3
ashcza <ashcon.zand@gmail.com>
2016-09-29 00:24:09 -0700

    Updated order of event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -21,9 +21,9 @@ class Event < ActiveRecord::Base
 	end
 
   def self.text_event
+    group = Group.find(1)
     @event = group.events.where("date > ?", Date.today).sort_by{|a| a.date}.first
     if ((Time.zone.now - @event.date).to_i / 1.day) == -2
-      group = Group.find(1)
       date = @event.date.strftime("%A, %b %e")
       time = @event.date.strftime("%l:%M %p")
 
