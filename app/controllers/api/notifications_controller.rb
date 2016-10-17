28f0a164cf87ae63d0ed26d6e38968f78f41b11e
ashcza <ashcon.zand@gmail.com>
2016-10-16 18:47:05 -0700

    Added array to red team none

--- app/controllers/api/notifications_controller.rb
+++ app/controllers/api/notifications_controller.rb
@@ -4,7 +4,7 @@ class Api::NotificationsController < ApplicationController
     black_team = Rsvp.where(event_id: notification_params[:event_id], color: "black").map {|rsvp| User.find(rsvp.user_id).name}
     white_team = Rsvp.where(event_id: notification_params[:event_id], color: "white").map {|rsvp| User.find(rsvp.user_id).name}
     red_team = Rsvp.where(event_id: notification_params[:event_id], color: "red").map {|rsvp| User.find(rsvp.user_id).name}
-    red_team = "none" if red_team.length == 0
+    red_team = ["none"] if red_team.length == 0
     event = Event.find(notification_params[:event_id])
     date = event.date.strftime("%A, %b %e")
     time = event.date.strftime("%l:%M %p")
